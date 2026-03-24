a6663639239eb8cc9a052d7196202a331c00f156
Elias Torres <eliato_@hotmail.com>
2026-03-24 16:47:26 -0600

    try to solved problem 1

--- public/index.php
+++ public/index.php
@@ -5,16 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Ruta a la raiz de Laravel (fuera de public_html, en ~/contratosaas/)
+define('LARAVEL_ROOT', __DIR__.'/../contratosaas');
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = LARAVEL_ROOT.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require LARAVEL_ROOT.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once LARAVEL_ROOT.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
